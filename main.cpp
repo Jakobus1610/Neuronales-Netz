238034af60a83c5a9fd110ddceea862c99e8039e
Jakobus1610 <99610119+Jakobus1610@users.noreply.github.com>
2024-02-19 13:28:15 +0000

    Erste Changes. Rechner wechels

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
+#include <cmath>
 
+const double EULER = 2.71828182845904523536;
+
+struct Node
+{
+    double value;
+    double bias;
+};
+
+double activationFunc(double value, double bias)
+{
+    return 1/(1+pow(EULER, -value + bias));
+}
+
+double evaluate(int NodeLayer, int NodeRow)
+{
+    double result;
+
+}
+
+int main()
+{
+
+}
